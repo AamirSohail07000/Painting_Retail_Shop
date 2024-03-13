057545ecb45a2b370094f192c949e83ba3b1d920
Aamir Sohail <155996028+AamirSohail07000@users.noreply.github.com>
2024-03-13 10:14:48 +0530

    Update script.js
    added eventlistener for scrolling

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
-
-/* we have to check if navbar is already open, if not & clicked it'll be open & vice versa */
+/* Check if navbar is already open, if not & clicked it'll be open & vice versa */
 if (bar) { 
     bar.addEventListener('click', () => {
         nav.classList.add('active')
-    }) /*  */
+    }) 
 }
 
 if (close) { 
-    bar.addEventListener('click', () => {
+    close.addEventListener('click', () => {
         nav.classList.remove('active')
-    }) /*  */
+    }) 
 }
 
 // JavaScript to loop through the images and apply the active class
@@ -25,3 +21,22 @@ setInterval(() => {
     index = (index + 1) % images.length;
     images[index].classList.add("active");
 }, 5000); // Change image every 5 seconds
+
+//Javascript to hide header when scrolling up
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let windowWidth = window.innerWidth; // Get window width
+
+    if (scrollTop > lastScrollTop && windowWidth > 599) { // Only hide header on larger screens
+        // Scrolling down
+        header.classList.add('header-hidden');
+    } else {
+        // Scrolling up or on small screens
+        header.classList.remove('header-hidden');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+});
